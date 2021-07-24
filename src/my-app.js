98f219dc4c1d45a194e09a7f3d84a0ec63fa716c
Melvin Ho <20740732+melvinhgf@users.noreply.github.com>
2021-07-24 13:35:59 +0800

    Update my-app.js

--- src/my-app.js
+++ src/my-app.js
@@ -97,7 +97,7 @@ class Resume extends PolymerElement {
             class="drawer-list"
             role="navigation"
           >
-            <a name="view1" href="[[rootPath]]privacy">Privacy</a>
+            <a name="view1" href="[[rootPath]]view1">Privacy</a>
             <a name="view2" href="[[rootPath]]view2">Cookie</a>
             <a name="view3" href="[[rootPath]]view3">Terms & Conditions</a>
             <a name="view4" href="[[rootPath]]view4">Acceptable Use</a>
