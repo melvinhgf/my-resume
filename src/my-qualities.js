30183cef092fb93076224daa0708370788a8fd3e
Melvin Ho <20740732+melvinhgf@users.noreply.github.com>
2021-07-24 19:33:04 +0800

    Update my-qualities.js

--- src/my-qualities.js
+++ src/my-qualities.js
@@ -23,12 +23,8 @@ class MyQualities extends PolymerElement {
       </style>
 
       <div class="card">
-<h1>Qualities</h1>
-<p>This acceptable use policy (&quot;Policy&quot;) sets forth the general guidelines and acceptable and prohibited uses of Our <a target="_blank" rel="nofollow" href="https://melvinhgf.business.site">website</a> (&quot;Website&quot; or &quot;Service&quot;) and any of its related products and services (collectively, &quot;Services&quot;).</p>
-<p>This Policy is a legally binding agreement between you (&quot;User&quot;, &quot;you&quot; or &quot;your&quot;) and this Website operator (&quot;Operator&quot;, &quot;we&quot;, &quot;us&quot; or &quot;our&quot;). By accessing and using the Website and Services, you acknowledge that you have read, understood, and agree to be bound by the terms of this Agreement.</p>
-<p>If you are entering into this Agreement on behalf of a business or other legal entity, you represent that you have the authority to bind such entity to this Agreement, in which case the terms &quot;User&quot;, &quot;you&quot; or &quot;your&quot; shall refer to such entity. If you do not have such authority, or if you do not agree with the terms of this Agreement, you must not accept this Agreement and may not access and use the Website and Services.</p>
-<p>You acknowledge that this Agreement is a contract between you and the Us, even though it is electronic and is not physically signed by you, and it governs your use of the Website and Services.</p>
-<h2>Prohibited Activities and Uses</h2>
+        <h1>Qualities</h1>
+        <h2>Prohibited Activities and Uses</h2>
 <p>You may not use the Website and Services to publish content or engage in activity that is illegal under applicable law, that is harmful to others, or that would subject us to liability, including, without limitation, in connection with any of the following, each of which is prohibited under this Policy:</p>
 <ul>
 <li>Distributing malware or other malicious code.</li>
@@ -45,54 +41,6 @@ class MyQualities extends PolymerElement {
 <li>Infringing the intellectual property or other proprietary rights of others.</li>
 <li>Facilitating, aiding, or encouraging any of the above activities through the Website and Services.</li>
 </ul>
-<h2>System Abuse</h2>
-<p>Any User in violation of the Website and Services security is subject to criminal and civil liability, as well as immediate account termination. Examples include, but are not limited to the following:</p>
-<ul>
-<li>Use or distribution of tools designed for compromising security of the Website and Services.</li>
-<li>Intentionally or negligently transmitting files containing a computer virus or corrupted data.</li>
-<li>Accessing another network without permission, including to probe or scan for vulnerabilities or breach security or authentication measures.</li>
-<li>Unauthorized scanning or monitoring of data on any network or system without proper authorization of the owner of the system or network.</li>
-</ul>
-<h2>Service Resources</h2>
-<p>You may not consume excessive amounts of the resources of the Website and Services or use the Website and Services in any way which results in performance issues or which interrupts the Services for other Users. Prohibited activities that contribute to excessive use, include without limitation:</p>
-<ul>
-<li>Deliberate attempts to overload the Website and Services and broadcast attacks (i.e. denial of service attacks).</li>
-<li>Engaging in any other activities that degrade the usability and performance of the Website and Services.</li>
-</ul>
-<h2>No Spam Policy</h2>
-<p>You may not use the Website and Services to send spam or bulk unsolicited messages.</p>
-<p>We maintain a zero tolerance policy for use of the Website and Services in any manner associated with the transmission, distribution or delivery of any bulk e-mail, including unsolicited bulk or unsolicited commercial e-mail, or the sending, assisting, or commissioning the transmission of commercial e-mail that does not comply with the U.S. CAN-SPAM Act of 2003 (&quot;SPAM&quot;).</p>
-<p>Your products or services advertised via SPAM (i.e. Spamvertised) may not be used in conjunction with the Website and Services.</p>
-<p>This provision includes, but is not limited to, SPAM sent via fax, phone, postal mail, email, instant messaging, or newsgroups.</p>
-<h2>Defamation and Objectionable Content</h2>
-<p>We value the freedom of expression and encourage Users to be respectful with the content they post. We are not a publisher of User content and are not in a position to investigate the veracity of individual defamation claims or to determine whether certain material, which we may find objectionable, should be censored.</p>
-<p>However, we reserve the right to moderate, disable or remove any content to prevent harm to others or to us or the Website and Services, as determined in our sole discretion.</p>
-<h2>Copyrighted Content</h2>
-<p>Copyrighted material must not be published via the Website and Services without the explicit permission of the copyright owner or a person explicitly authorized to give such permission by the copyright owner.</p>
-<p>Upon receipt of a claim for copyright infringement, or a notice of such violation, we will immediately run full investigation and, upon confirmation, will notify the person or persons responsible for publishing it and, in our sole discretion, will remove the infringing material from the Website and Services. We may terminate the Service of Users with repeated copyright infringements. Further procedures may be carried out if necessary.</p>
-<p>We will assume no liability to any User of the Website and Services for the removal of any such material. If you believe your copyright is being infringed by a person or persons using the Website and Services, please get in touch with us to report copyright infringement.</p>
-<h2>Security</h2>
-<p>You take full responsibility for maintaining reasonable security precautions for your account.</p>
-<p>You are responsible for protecting and updating any login account provided to you for the Website and Services. You must protect the confidentiality of your login details, and you should change your password periodically.</p>
-<h2>Enforcement</h2>
-<p>We reserve our right to be the sole arbiter in determining the seriousness of each infringement and to immediately take corrective actions, including but not limited to:</p>
-<ul>
-<li>Suspending or terminating your Service with or without notice upon any violation of this Policy. Any violations may also result in the immediate suspension or termination of your account.</li>
-<li>Disabling or removing any content which is prohibited by this Policy, including to prevent harm to others or to us or the Website and Services, as determined by us in our sole discretion.</li>
-<li>Reporting violations to law enforcement as determined by us in our sole discretion.</li>
-<li>A failure to respond to an email from our abuse team within 2 days, or as otherwise specified in the communication to you, may result in the suspension or termination of your account.</li> 
-</ul>
-<p>Suspended and terminated User accounts due to violations will not be re-activated.</p>
-<p>Nothing contained in this Policy shall be construed to limit our actions or remedies in any way with respect to any of the prohibited activities. In addition, we reserve at all times all rights and remedies available to us with respect to such activities at law or in equity.</p>
-<h2>Reporting Violations</h2>
-<p>If you have discovered and would like to report a violation of this Policy, please contact us immediately. We will investigate the situation and provide you with full assistance.</p>
-<h2>Changes and Amendments</h2>
-<p>We reserve the right to modify this Policy or its terms relating to the Website and Services at any time, effective upon posting of an updated version of this Policy on the Website. When we do, we will revise the updated date at the bottom of this page. Continued use of the Website and Services after any such changes shall constitute your consent to such changes.</p>
-<h2>Acceptance of This Policy</h2>
-<p>You acknowledge that you have read this Policy and agree to all its terms and conditions. By accessing and using the Website and Services you agree to be bound by this Policy. If you do not agree to abide by the terms of this Policy, you are not authorized to access or use the Website and Services.</p>
-<h2>Contacting Us</h2>
-<p>If you would like to contact us to understand more about this Policy or wish to contact us concerning any matter relating to it, you may do so via this <a target="_blank" rel="nofollow" href= "https://www.cognitoforms.com/MELVINHGFNETYourOnlineSolutionsProvider/ContactUs">link</a>, Contact Us page or send an email to &#97;b&#117;&#115;&#101;&#64;me&#108;vin&#104;&#103;f.ne&#116;.</p>
-<p>This document was last updated on September 13, 2019</p>
       </div>
     `;
   }
