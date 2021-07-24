7b930bcc0a510d240edc1e0c1295f165445bc392
Melvin Ho <20740732+melvinhgf@users.noreply.github.com>
2021-07-24 19:51:24 +0800

    Update my-qualities.js

--- src/my-qualities.js
+++ src/my-qualities.js
@@ -24,22 +24,23 @@ class MyQualities extends PolymerElement {
 
       <div class="card">
         <h1>Qualities</h1>
-        <h2>Prohibited Activities and Uses</h2>
-<p>You may not use the Website and Services to publish content or engage in activity that is illegal under applicable law, that is harmful to others, or that would subject us to liability, including, without limitation, in connection with any of the following, each of which is prohibited under this Policy:</p>
 <ul>
-<li>Distributing malware or other malicious code.</li>
-<li>Disclosing sensitive personal information about others.</li>
-<li>Collecting, or attempting to collect, personal information about third parties without their knowledge or consent.</li>
-<li>Distributing pornography or adult related content.</li>
-<li>Promoting or facilitating prostitution or any escort services.</li>
-<li>Hosting, distributing or linking to child pornography or content that is harmful to minors.</li>
-<li>Promoting or facilitating gambling, violence, terrorist activities or selling weapons or ammunition.</li>
-<li>Engaging in the unlawful distribution of controlled substances, drug contraband or prescription medications.</li>
-<li>Managing payment aggregators or facilitators such as processing payments on behalf of other businesses or charities.</li>
-<li>Facilitating pyramid schemes or other models intended to seek payments from public actors.</li>
-<li>Threatening harm to persons or property or otherwise harassing behavior.</li>
-<li>Infringing the intellectual property or other proprietary rights of others.</li>
-<li>Facilitating, aiding, or encouraging any of the above activities through the Website and Services.</li>
+<li>Empathy</li>
+<li>Impartial</li>
+<li>People-oriented</li>
+<li>Analytical</li>
+<li>Meticulous</li>
+<li>Adaptable</li>
+<li>Easy-going</li>
+<br>
+<li>Freedom</li>
+<li>Transparent</li>
+
+<li>Communicator</li>
+<li>Mentor</li>
+<li>Partner</li>
+<li>Gets-Work-Done</li>
+
 </ul>
       </div>
     `;
