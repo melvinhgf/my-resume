bae311db8f902494cae2988e7e084561369b81d0
Melvin Ho <20740732+melvinhgf@users.noreply.github.com>
2021-07-24 19:03:13 +0800

    Update my-about.js

--- src/my-about.js
+++ src/my-about.js
@@ -19,6 +19,12 @@ class MyAbout extends PolymerElement {
           display: block;
           padding: 10px;
         }
+        
+        .a {
+          colour: red;
+          text-decoration: none;
+         }
+         
       </style>
       <div class="card">
         <h1>About</h1>
@@ -28,13 +34,8 @@ class MyAbout extends PolymerElement {
         <p>Citizenship: <b>Singaporean</b></p>
         <p>Academic Qualifications: <b>Graduate</b></p>
         <p>Marital Status: <i>Please contact me directly.</i></p>
-        <p>Contacts: <i>Please use the Contact form</b></p>
-        <p>Social: <a href="https://facebook.com/melvinhgf" target="_blank" text-decoration="none" >Facebook</a> and <a href="https://linkedin.com/in/melvinhgf" target="_blank" text-decoration="none" >LinkedIn</a></p>
-        
-        <p>Melvin is an explorer of life, a person who loves to defeat challenges that come his way and climb new heights in life.</p>
-        <p>He loves - and equally capable of - interacting and exchanging ideas with people of all ages. He makes a patient and nurturing mentor who always brings an underdog's positive traits.</p>
-        <p>Melvin has high standards for things and expects everyone, including himself, to meet them.</p>
-        <p>Years of overcoming challenges have formed Melvin's mission in life: <b><i>“Using Life To Influence Lives”</i></b>. He is determined to live out this mission.</p> 
+        <p>Contacts: <i>Please use the Contact form</i></p>
+        <p>Social: <a href="https://facebook.com/melvinhgf" target="_blank"><b>Facebook</b></a> and <a href="https://linkedin.com/in/melvinhgf" target="_blank" ><b>LinkedIn</b></a></p>
       </div>
     `;
   }
