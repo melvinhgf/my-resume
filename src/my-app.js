0b287759c14d976df8b34af82bc0344b40043755
Melvin Ho <20740732+melvinhgf@users.noreply.github.com>
2021-07-24 15:14:08 +0800

    Update my-app.js

--- src/my-app.js
+++ src/my-app.js
@@ -90,7 +90,7 @@ class MyApp extends PolymerElement {
       <app-drawer-layout fullbleed="" narrow="{{narrow}}">
         <!-- Drawer content -->
         <app-drawer id="drawer" slot="drawer" swipe-open="[[narrow]]">
-          <app-toolbar><a href="/">My Resume</a></app-toolbar>
+          <app-toolbar><a href="/" text-decoration="none">My Resume</a></app-toolbar>
           <iron-selector
             selected="[[page]]"
             attr-for-selected="name"
